690356dadca179d7ba03cb49f9f450b143a7424f
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 02:47:11 +0600

    first commit

---
+++ DemoArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[3];
+            Console.WriteLine(number[0]);
+            Console.WriteLine(number[1]); 
+            Console.WriteLine(number[2]);
+
+            number[1] = 10;
+
+            Console.WriteLine(number[0]);
+            Console.WriteLine(number[1]);
+            Console.WriteLine(number[2]);
+
+            bool[] flag = new bool[10];
+            flag[4] = true;
+            Console.WriteLine(flag[0]);
+            Console.WriteLine(flag[4]);
+          
+
+        }
+    }
+}
